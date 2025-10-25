516a00d4f34b2e78e1473bd606970e23ff597601
Асланбек Кашежев <adiga83@mail.ru>
2025-10-25 10:12:34 +0300

    Update site - 2025-10-25 10:12:34

---
+++ update-site.ps1
@@ -0,0 +1,15 @@
+# Переходим в папку проекта
+cd "C:\Users\a.kashezhev\Desktop\МОЙ_site_prizvanie"
+
+# Добавляем все изменённые файлы
+git add .
+
+# Создаём коммит с текущей датой и временем
+$commitMessage = "Update site - " + (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
+git commit -m $commitMessage
+
+# Отправляем изменения на GitHub
+git push origin main
+
+Write-Host "Сайт обновлён! Через минуту изменения будут видны на GitHub Pages:"
+Write-Host "https://Aslanbek-AFK.github.io/my_site/"
